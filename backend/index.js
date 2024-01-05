10a602942ed70130ff7af70be4c46f0ad6e7d269
Mohit Jangid <77240127+Mohitjangid097@users.noreply.github.com>
2024-01-05 15:10:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,9 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+app.get('/',(req,res)=>{
+  res.send("server is running")
+})
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
